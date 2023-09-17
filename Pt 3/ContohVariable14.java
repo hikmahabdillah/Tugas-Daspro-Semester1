6f815cb1348ea88acce183b5451f96620b69a9ca
hikmahabdillah <hikmahaldrin44@gmail.com>
2023-09-17 19:30:58 +0700

    menambahkan file baru

---
+++ Pt 3/ContohVariable14.java
@@ -0,0 +1,15 @@
+public class ContohVariable14{
+  public static void main(String[] args) {
+    String hobby = "Bermain PS";
+    boolean pandai = true;
+    char jenisKelamin = 'L';
+    byte umur = 18;
+    double ipk = 3.95, tinggiBadan = 1.63;
+    System.out.println("Hobby saya: " + hobby);
+    System.out.println("Apakah pandai?" + pandai);
+    System.out.println("Jenis Kelamin: " + jenisKelamin);
+    System.out.println("Umurku saat ini: " + umur);
+    System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", ipk, tinggiBadan));
+
+  }
+}
