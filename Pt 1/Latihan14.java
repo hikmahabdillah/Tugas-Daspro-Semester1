19f0cc91ee077265764fed51dbe06d1f87366d90
hikmahabdillah <hikmahaldrin44@gmail.com>
2023-09-17 20:34:07 +0700

    fix

---
+++ Pt 1/Latihan14.java
@@ -0,0 +1,10 @@
+/*
+LATIHAN 14 * 
+ */
+
+public class Latihan14 {
+  public static void main(String[] args) {
+    System.out.print("Nama saya Hikmah Aldrin Abdillah");
+    // System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+  }
+}
---
+++ Pt 1/Tugas14.java
@@ -0,0 +1,16 @@
+public class Tugas14 {
+  public static void main(String[] args) {
+    String name = "Hikmah Aldrin Abdillah";
+    int absen = 14;
+    String kelas = "1F";
+    String prodi = "D-IV TEKNIK INFORMATIKA";
+    String jurusan = "TEKNOLOGI INFORMASI";
+
+    System.out.println("***** IDENTITAS DIRI *****");
+    System.out.println("Nama             : "+ name);
+    System.out.println("Absen            : "+ absen);
+    System.out.println("Kelas            : "+ kelas);
+    System.out.println("Prodi            : "+ prodi);
+    System.out.println("Jurusan          : "+ jurusan);
+  }
+}
