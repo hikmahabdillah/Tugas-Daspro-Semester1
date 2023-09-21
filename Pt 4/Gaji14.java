5e6de11478a5f899045e4b14e53ecf91ead7a1dd
hikmahabdillah <hikmahaldrin44@gmail.com>
2023-09-21 14:37:23 +0700

    menambahkan gaji14.java

---
+++ Pt 4/Gaji14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Gaji14 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+      int jmlMasuk, jmlTdkMasuk, totGaji;
+      int gaji = 40000, potGaji=25000;
+
+      System.out.println("Masukkan Jumlah Hari Masuk Kerja Anda ");
+      jmlMasuk=input.nextInt();
+      System.out.println("Masukkan Jumlah Hari Tidak Masuk Kerja Anda ");
+      jmlTdkMasuk=input.nextInt();
+
+      totGaji=(jmlMasuk*gaji)-(jmlTdkMasuk*potGaji);
+      System.out.println("Gaji yang anda terima adalah "+ totGaji);
+  }
+}
--- Pt 4/Lingkaran14.java
+++ Pt 4/Lingkaran14.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-
 public class Lingkaran14 {
   public static void main(String[] args) {
     Scanner input  = new Scanner(System.in);
