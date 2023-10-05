7a59023a6554de62cdd2836b5229a21399d7f5dd
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-05 11:55:36 +0700

    memodifikasi syntax if else menjadi ternary operator

--- Pt 5/PemilihanPercobaan1No14.java
+++ Pt 5/PemilihanPercobaan1No14.java
@@ -10,10 +10,17 @@ public static void main(String[] args) {
     System.out.println("Masukkan angka :  ");
     int angka = input14.nextInt();
      
+     /* 
     if (angka % 2 == 0)
       System.out.println("Angka "+ angka + " bilangan genap");
     else 
       System.out.println("Angka "+ angka + " bilangan ganjil");
+    */
     
+    // ternary operator
+    // variable = (condition) ? expressionTrue :  expressionFalse;
+    // karena sebenarnya ternary operator adalah short hand dari pemilihan, hanya saja beda penulisan syntax saja. tetapi 
+    String hasil = (angka % 2 == 0) ? ("Angka "+ angka + " bilangan genap") : ("Angka "+ angka + " bilangan ganjil");
+    System.out.println(hasil);
   } 
 }
