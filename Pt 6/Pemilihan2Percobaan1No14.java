dcab9f1d07667e04bd711521a988d51b60b5f365
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-12 13:20:02 +0700

    modifikasi1 Pemilihan2percobaan1no14

--- Pt 6/Pemilihan2Percobaan1No14.java
+++ Pt 6/Pemilihan2Percobaan1No14.java
@@ -10,6 +10,8 @@ public class Pemilihan2Percobaan1No14 {
     if ((tahun % 4) == 0) {
       if ((tahun % 100) != 0) {
         System.out.println("Tahun Kabisat");
+      }else{
+        System.out.println("Bukan Tahun Kabisat");
       }
     } else {
       System.out.println("Bukan Tahun Kabisat");
