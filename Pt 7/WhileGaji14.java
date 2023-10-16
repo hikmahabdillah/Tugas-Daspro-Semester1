ea5a1d9a8c2bd8ebbcb9a1cdad3d6fd226ab1c2a
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-16 15:43:52 +0700

    add file whilegaji

---
+++ Pt 7/WhileGaji14.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class WhileGaji14 {
+  public static void main(String[] args) {
+        Scanner scan14 = new Scanner(System.in);
+
+        int jumlahkaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0 ;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahkaryawan = scan14.nextInt();
+
+        int i = 0;
+
+            while (i < jumlahkaryawan) {
+            
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan"); 
+            System.out.print ("Masukkan jabatan karyawan ke-" + (i+1) + ": "); 
+            jabatan = scan14.next();
+            System.out.print("Masukkan jumlah jam lembur : ");
+            jumlahJamLembur = scan14.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            }
+            else if (jabatan.equalsIgnoreCase("manajer")) { 
+                gajiLembur = jumlahJamLembur * 100000;
+            }
+            else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }       
+
+            totalGajiLembur += gajiLembur;
+          }
+
+          System.out.println("Total gaji lembur : " + totalGajiLembur);
+          }   
+    
+}
+
+
