bc2a67d37c67d2840cbb494338db1fc6a5c1bdfb
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-16 15:02:56 +0700

    memodifikasi file tsb, dengan menambahkan rata-rata dari kelipatan

--- Pt 7/ForKelipatan14.java
+++ Pt 7/ForKelipatan14.java
@@ -7,7 +7,7 @@ public class ForKelipatan14 {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
-    int kelipatan, jumlah= 0, counter= 0;
+    int kelipatan, average=0, jumlah= 0, counter= 0;
 
     System.out.print("Masukkan bilangan kelipatan 1-9: ");
     kelipatan = scan.nextInt();
@@ -18,6 +18,10 @@ public class ForKelipatan14 {
         counter++;
       }
     }
+    // operasi untuk menghitung rata rata
+    average = (kelipatan * jumlah) / counter;
+
+    System.out.println("Rata-rata dari seluruh bilangan kelipatan adalah " + average);
 
     System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
     System.out.printf("Total bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
