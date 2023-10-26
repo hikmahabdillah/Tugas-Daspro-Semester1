b84ae6d868ab108aed706b68a46db908d06a6a1c
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-26 11:49:30 +0700

    modif file arraybilangan14 change to for

--- Pt 9/ArrayBilangan14.java
+++ Pt 9/ArrayBilangan14.java
@@ -10,10 +10,14 @@ public class ArrayBilangan14 {
     bil[2] = -7;
     bil[3] = 17;
 
-    System.out.println(bil[0]);
-    System.out.println(bil[1]);
-    System.out.println(bil[2]);
-    System.out.println(bil[3]);
+    // System.out.println(bil[0]);
+    // System.out.println(bil[1]);
+    // System.out.println(bil[2]);
+    // System.out.println(bil[3]);
+
+    for (int i = 0; i <= 4; i++) {
+      System.out.println(bil[i]);
+    }
 
   }
 }
