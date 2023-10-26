68c1469a3df7b809f82fd1198cc88b35c35c424f
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-26 12:27:42 +0700

    add file bublesortexample14

---
+++ Pt 9/BubbleSortExample14.java
@@ -0,0 +1,29 @@
+/**
+ * BubbleSortExample14
+ */
+public class BubbleSortExample14 {
+
+  public static void main(String[] args) {
+
+    int[] intData = { 34, 18, 87, 72, 32, 54, 43 };
+    int temp = 0;
+
+    // ASCENDING
+    for (int i = 0; i < intData.length; i++) {
+      for (int j = 1; j < intData.length - i; j++) {
+        if (intData[j - 1] > intData[j]) {
+          // swap elemen
+          temp = intData[j];
+          intData[j] = intData[j - 1];
+          intData[j - 1] = temp;
+        }
+      }
+    }
+
+    System.out.println("Hasil Pengurutan:");
+    for (int i = 0; i < intData.length; i++) {
+      System.out.println(intData[i]);
+    }
+
+  }
+}
