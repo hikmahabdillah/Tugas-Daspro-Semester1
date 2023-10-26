c25f4f31aa67cc2bb435feca5eb3375170d36f41
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-26 12:01:21 +0700

    modif arraynilai14 , add if else

--- Pt 9/ArrayNilai14.java
+++ Pt 9/ArrayNilai14.java
@@ -5,14 +5,27 @@ public class ArrayNilai14 {
     int[] nilaiAkhir = new int[10];
     Scanner sc = new Scanner(System.in);
 
-    for (int i = 0; i < 10; i++) {
+    // for (int i = 0; i < 10; i++) {
+    // System.out.print("Masukkan nilai akhir ke- " + i + ": ");
+    // nilaiAkhir[i] = sc.nextInt();
+    // }
+
+    // for (int i = 0; i < 10; i++) {
+    // System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+    // }
+
+    for (int i = 0; i < nilaiAkhir.length; i++) {
       System.out.print("Masukkan nilai akhir ke- " + i + ": ");
       nilaiAkhir[i] = sc.nextInt();
     }
 
-    for (int i = 0; i < 10; i++) {
-      System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+    // modifikasi no 4
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+      if (nilaiAkhir[i] > 70) {
+        System.out.println("Mahasiswa ke-" + i + " lulus!");
+      } else {
+        System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+      }
     }
-
   }
 }
