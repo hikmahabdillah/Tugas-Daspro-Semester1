6f5a01b0343c0eafe1aa96b5c0b60b94697052bf
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-26 14:42:55 +0700

    modif file linear search 14 no 2

--- Pt 9/LinearSearch14.java
+++ Pt 9/LinearSearch14.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class LinearSearch14 {
   public static void main(String[] args) {
-    int[] arrayInt = { 34, 18, 26, 48, 72, 20, 56, 63 };
-    int key = 20;
+    Scanner sc = new Scanner(System.in);
+
+    int key = 0;
     int hasil = 0;
+    int n_elemen = 0;
+
+    System.out.print("Masukkan jumlah elemen array : ");
+    n_elemen = sc.nextInt();
+
+    int[] arrayInt = new int[n_elemen];
+
+    for (int i = 0; i < arrayInt.length; i++) {
+      System.out.print("Masukkan elemen array ke -" + i + " : ");
+      arrayInt[i] = sc.nextInt();
+    }
+
+    System.out.print("Msukkan key yang ingin dicari : ");
+    key = sc.nextInt();
+
     for (int i = 0; i < arrayInt.length; i++) {
       if (arrayInt[i] == key) {
         hasil = i;
