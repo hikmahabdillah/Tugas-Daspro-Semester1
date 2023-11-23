ca5ab95cb1f76fc585fe62b2e2446be1bc07457e
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-11-23 14:49:55 +0700

    add Percobaan3

---
+++ Pt 13/UcapanTerimakasih_14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UcapanTerimakasih_14 {
+  public static String PenerimaUcapan() {
+    Scanner Input14 = new Scanner(System.in);
+    System.out.println("Tuliskan NAMA orang yang ingin Anda berikan ucapan: ");
+    String namaOrang = Input14.nextLine();
+    Input14.close();
+    return namaOrang;
+  }
+
+  public static void UcapanTerimakasih() {
+
+    String nama = PenerimaUcapan();
+    System.out.println("Thank you " + nama + " for being the best teacher in the world. \n"
+        + "You  inspired in me a love for learning and made me feel like I could ask you anything.");
+
+  }
+
+  public static void main(String[] args) {
+    UcapanTerimakasih();
+  }
+}
